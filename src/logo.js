35e04aa10137d55a8d1acb092b29acde67871508
janardhanarajesh <140391864+janardhanarajesh@users.noreply.github.com>
2024-07-06 15:49:07 +0530

    Add files via upload

---
+++ src/logo.js
@@ -0,0 +1,12 @@
+function Logo()
+{
+setTimeout( function move(){
+    window.location.href="/register"
+},1800)
+    return(
+        <div id="body">
+
+        </div>
+    )
+}
+export default Logo
